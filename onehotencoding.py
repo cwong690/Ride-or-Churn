955b71bffcfe4f5712bfa063e720c014283fee7b
Tyler Woods <tyjordan.woods@gmail.com>
2020-04-24 10:36:31 -0700

    add ohe

---
+++ onehotencoding.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+def set_ohe(df:pd.DataFrame, col_name:str):
+    for val in df[col_name].value_counts().index:
+        df[f"{col_name}: {val}"] = df[col_name].map(lambda x: 1.0 if x==val else 0.0 )
+    return df
